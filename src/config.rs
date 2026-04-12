e5089b4303c0a4456c4d846784ee491df0b09cce
ExxML <andywu9509@gmail.com>
2026-04-11 21:57:12 -0700

    chore: decrease speeds

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
-pub const MOVE_SPEED_PX_PER_SEC: f64 = 1200.0;
-pub const SCROLL_SPEED_UNITS_PER_SEC: f64 = 1000.0;
+pub const MOVE_SPEED_PX_PER_SEC: f64 = 1000.0;
+pub const SCROLL_SPEED_UNITS_PER_SEC: f64 = 500.0;
 pub const FAST_MULTIPLIER: f64 = 2.0;
 pub const SLOW_MULTIPLIER: f64 = 0.3;
 pub const TICK_RATE_HZ: u64 = 120;
