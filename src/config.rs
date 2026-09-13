cc97397aa33d0c9af09cc09ee635a02f229ecd6f
ExxML <andywu9509@gmail.com>
2026-09-12 19:02:45 -0700

    chore: make mode overlay line smaller

--- src/config.rs
+++ src/config.rs
@@ -81,9 +81,9 @@ pub enum ModeOverlayPos {
     Bottom,
 }
 pub const MODE_OVERLAY_POSITION: ModeOverlayPos = ModeOverlayPos::Bottom;
-pub const MODE_OVERLAY_THICKNESS_MONITOR_FRACTION: f64 = 0.006; // Mode line thickness as a fraction of the monitor's vertical dimension
+pub const MODE_OVERLAY_THICKNESS_MONITOR_FRACTION: f64 = 0.004; // Mode line thickness as a fraction of the monitor's vertical dimension
 pub const MODE_OVERLAY_NORMAL_COLOR: [u8; 4] = [122, 162, 246, 255];
-pub const MODE_OVERLAY_INSERT_COLOR: [u8; 4] = [158, 206, 107, 255];
+pub const MODE_OVERLAY_INSERT_COLOR: [u8; 4] = [162, 246, 122, 255];
 
 // Grid overlay line appearance
 pub const DEFAULT_GRID_ENABLED: bool = false; // Whether the grid overlay is enabled by default
